3817b6da5cab291d08fca647ca5995cc669b59f2
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-07-25 21:24:01 +0200

    fixs

--- js/task-10.js
+++ js/task-10.js
@@ -17,7 +17,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
 function createBoxes() {
   let collectionMarkup = "";
   let divSize = 30;
@@ -35,4 +34,5 @@ function createBoxes() {
 
 function destroyBoxes() {
   refs.containerDiv.innerHTML = "";
+  refs.input.value = "";
 }
